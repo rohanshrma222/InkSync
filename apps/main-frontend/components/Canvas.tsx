71fe5ca5949a454675d64efb814b3016064d7186
unknown <hackerviruse04@gmail.com>
2025-03-21 11:45:56 +0530

    .

--- apps/main-frontend/components/Canvas.tsx
+++ apps/main-frontend/components/Canvas.tsx
@@ -2,49 +2,58 @@
 import { Circle, Pencil, RectangleHorizontalIcon } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 import { IconButton } from "./ui/iconButton";
+import { Game } from "@/components/draw/Game";
 
-export type Tool = "circle" | "rect" | "pencil"
+export type Tool = "circle" | "rect" | "pencil";
+
 export function Canvas({
     roomId,
     socket
-}:{
-    socket: WebSocket
+}: {
+    socket: WebSocket;
     roomId: string;
-}){
-
+}) {
     const canvasRef = useRef<HTMLCanvasElement>(null);
-    const [game,setGame] = useState<Game>();
-    const [selectedTool,setSelectedTool] = useState<Tool>("circle")
+    const [game, setGame] = useState<Game>();
+    const [selectedTool, setSelectedTool] = useState<Tool>("circle")
 
-    useEffect(()=>{
-      game?.setTool(selectedTool);
-    },[selectedTool,game]);
+    useEffect(() => {
+        game?.setTool(selectedTool);
+    }, [selectedTool, game]);
 
-    useEffect(()=>{
-        if(canvasRef.current){
-            const g = new Game(canvasRef.current,roomId,socket)
+    useEffect(() => {
+
+        if (canvasRef.current) {
+            const g = new Game(canvasRef.current, roomId, socket);
             setGame(g);
+
+            return () => {
+                g.destroy();
+            }
         }
 
-    },[canvasRef]);
 
-    return <div className="h-screen overflow:hidden">
-           <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight}></canvas>
-           <Topbar setSelectedTool={setSelectedTool} selectedTool={selectedTool} />
-           </div>
+    }, [canvasRef]);
+
+    return <div style={{
+        height: "100vh",
+        overflow: "hidden"
+    }}>
+        <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight}></canvas>
+        <Topbar setSelectedTool={setSelectedTool} selectedTool={selectedTool} />
+    </div>
 }
 
-function Topbar({selectedTool,setSelectedTool}:{
-    selectedTool:Tool,
+function Topbar({selectedTool, setSelectedTool}: {
+    selectedTool: Tool,
     setSelectedTool: (s: Tool) => void
-}){
-    return(
-        <div style={{
+}) {
+    return <div style={{
             position: "fixed",
             top: 10,
             left: 10
         }}>
-             <div className="flex gap-t">
+            <div className="flex gap-t">
                 <IconButton 
                     onClick={() => {
                         setSelectedTool("pencil")
@@ -60,5 +69,4 @@ function Topbar({selectedTool,setSelectedTool}:{
                 }} activated={selectedTool === "circle"} icon={<Circle />}></IconButton>
             </div>
         </div>
-    )
 }
