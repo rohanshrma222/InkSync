ca260f1e4d5aa3559b3f1f20e5d1dd793e9c1b5c
unknown <hackerviruse04@gmail.com>
2025-05-09 20:30:51 +0530

    added join room

--- apps/main-frontend/app/room/page.tsx
+++ apps/main-frontend/app/room/page.tsx
@@ -48,7 +48,7 @@ export default function RoomPage() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gradient-to-r from-white-100 to-purple-500">
             <div className="bg-white p-6 rounded-lg shadow-2xl w-96">
-                <h1 className="text-2xl flex justify-center font-black pb-4">Create a Room</h1>
+                <h1 className="text-2xl flex justify-center font-black pb-4">Create Room</h1>
                 <label className="block text-gray-700 font-black">Room Name</label>
                 <div className="flex items-center border border-black rounded-md px-3 py-2">  
                     <House className="text-gray-500 mr-2" size={20} />
@@ -67,6 +67,26 @@ export default function RoomPage() {
                     className="w-full mt-2 py-2 bg-[#9950af] text-white rounded-md hover:bg-[#9950af]/80"
                 >
                     Create Room
+                </button>
+                <h1 className="text-2xl flex justify-center font-black pb-4  pt-5">Join Room</h1>
+                <label className="block text-gray-700 font-black">Room Name</label>
+                <div className="flex items-center border border-black rounded-md px-3 py-2">  
+                    <House className="text-gray-500 mr-2" size={20} />
+                    <input
+                        type="text"
+                        name="name"
+                        className="w-full outline-none bg-transparent"
+                        placeholder="piggies"
+                        required
+                        value={roomName}
+                        onChange={(e) => setRoomName(e.target.value)}
+                    />
+                </div>
+                <button
+                    onClick={createRoom}
+                    className="w-full mt-2 py-2 bg-[#9950af] text-white rounded-md hover:bg-[#9950af]/80"
+                >
+                    Join Room
                 </button>
             </div> 
         </div>
